d3847613230815ee81b8de28375814606376671b
Naim <skylike2904@gmail.com>
2023-05-19 00:20:05 +0600

    up

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 // importing libraries:
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 
 // importing stylesheets:
 import "./index.css";
-
-// importing remixicon
 import "remixicon/fonts/remixicon.css";
 
 // importing components:
 import App from "./App";
-// importing context provider
-// import { AppContextProvider } from "./Context/Context";
-import { AppContextProvider } from "./Billal's file/Context/Context";
 
 // creating root:
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -21,10 +15,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // rendering root:
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
-    {/* <AppContextProvider> */}
     <App />
-    {/* </AppContextProvider> */}
-    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
